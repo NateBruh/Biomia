3c3984b2fb32ba3b32f1919bc03428762bf53f0f
naten <naten@NATHANS-PC.Davolink>
2020-08-20 15:55:43 +0900

    wasteland biome fixes

--- src/main/java/net/mcreator/biomia/world/biome/BiomeWasteland.java
+++ src/main/java/net/mcreator/biomia/world/biome/BiomeWasteland.java
@@ -1,14 +1,17 @@
 
 package net.mcreator.biomia.world.biome;
 
+import net.minecraftforge.fml.relauncher.SideOnly;
+import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.common.BiomeManager;
 
 import net.minecraft.world.gen.feature.WorldGenAbstractTree;
 import net.minecraft.world.biome.Biome;
-import net.minecraft.init.Blocks;
+import net.minecraft.util.math.BlockPos;
 
+import net.mcreator.biomia.block.BlockWastelandStandstone;
 import net.mcreator.biomia.block.BlockWastelandSand;
 import net.mcreator.biomia.ElementsBiomia;
 
@@ -34,25 +37,44 @@ public class BiomeWasteland extends ElementsBiomia.ModElement {
 	}
 	static class BiomeGenCustom extends Biome {
 		public BiomeGenCustom() {
-			super(new Biome.BiomeProperties("Wasteland").setRainfall(0.25F).setBaseHeight(0.1F).setHeightVariation(0.1F).setTemperature(1F));
+			super(new Biome.BiomeProperties("Wasteland").setRainfall(0.25F).setBaseHeight(0.1F).setWaterColor(-9670795).setHeightVariation(0.25F)
+					.setTemperature(1F));
 			setRegistryName("wasteland");
 			topBlock = BlockWastelandSand.block.getDefaultState();
-			fillerBlock = Blocks.DIRT.getStateFromMeta(0);
+			fillerBlock = BlockWastelandStandstone.block.getDefaultState();
 			decorator.generateFalls = false;
 			decorator.treesPerChunk = 0;
 			decorator.flowersPerChunk = 0;
 			decorator.grassPerChunk = 0;
-			decorator.deadBushPerChunk = 4;
+			decorator.deadBushPerChunk = 5;
 			decorator.mushroomsPerChunk = 0;
 			decorator.bigMushroomsPerChunk = 0;
 			decorator.reedsPerChunk = 0;
-			decorator.cactiPerChunk = 2;
-			decorator.sandPatchesPerChunk = 1;
+			decorator.cactiPerChunk = 0;
+			decorator.sandPatchesPerChunk = 0;
 			decorator.gravelPatchesPerChunk = 0;
 			this.spawnableMonsterList.clear();
 			this.spawnableCreatureList.clear();
 			this.spawnableWaterCreatureList.clear();
 			this.spawnableCaveCreatureList.clear();
+		}
+
+		@SideOnly(Side.CLIENT)
+		@Override
+		public int getGrassColorAtPos(BlockPos pos) {
+			return -8816263;
+		}
+
+		@SideOnly(Side.CLIENT)
+		@Override
+		public int getFoliageColorAtPos(BlockPos pos) {
+			return -8816263;
+		}
+
+		@SideOnly(Side.CLIENT)
+		@Override
+		public int getSkyColorByTemp(float currentTemperature) {
+			return -2696736;
 		}
 
 		@Override
