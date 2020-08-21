7d0c3a993205aa7065615552fcbedd1c9f199eb7
jessi <jessi@DESKTOP-NONKDQC>
2020-08-21 17:21:33 -0400

    Removed Forest

--- src/main/java/net/mcreator/biomia/util/LootTableWastelandCrashSite.java
+++ src/main/java/net/mcreator/biomia/util/LootTableWastelandCrashSite.java
@@ -1,9 +1,15 @@
 
 package net.mcreator.biomia.util;
 
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+import net.minecraft.world.storage.loot.LootTableList;
+import net.minecraft.util.ResourceLocation;
+
+import net.mcreator.biomia.ElementsBiomia;
+
 @ElementsBiomia.ModElement.Tag
 public class LootTableWastelandCrashSite extends ElementsBiomia.ModElement {
-
 	public LootTableWastelandCrashSite(ElementsBiomia instance) {
 		super(instance, 9);
 	}
@@ -12,5 +18,4 @@ public class LootTableWastelandCrashSite extends ElementsBiomia.ModElement {
 	public void init(FMLInitializationEvent event) {
 		LootTableList.register(new ResourceLocation("biomia", "crashsite"));
 	}
-
 }
